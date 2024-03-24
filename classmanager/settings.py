32d0e502650cff2b936b4a267b3913cdcc48e165
CHOCKAN <149298690+chockan@users.noreply.github.com>
2024-03-24 16:39:15 +0530

    Update settings.py

--- classmanager/settings.py
+++ classmanager/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+MEDIA_URL='/images/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
-MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
